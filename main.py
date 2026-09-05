3b93fe4915e06f284590c68a13838c6b43d6f3ac
WilliamFrench-GB <william.h.b.french@gmail.com>
2026-09-05 16:34:40 +0100

    Add candles data and calculateAverageClose function

---
+++ main.py
@@ -0,0 +1,27 @@
+# The 12 candles are a toy dataset.
+# Historical data would be needed to draw real conclusions. 
+candles = [
+    {"open": 125, "high": 175, "low": 110, "close": 150},
+    {"open": 135, "high": 185, "low": 120, "close": 160},
+    {"open": 115, "high": 165, "low": 110, "close": 140},
+    {"open": 145, "high": 195, "low": 130, "close": 170},
+    {"open": 155, "high": 205, "low": 140, "close": 180},
+    {"open": 144, "high": 178, "low": 143, "close": 155},
+    {"open": 190, "high": 190, "low": 180, "close": 185},
+    {"open": 210, "high": 260, "low": 190, "close": 200},
+    {"open": 130, "high": 170, "low": 130, "close": 170},
+    {"open": 150, "high": 160, "low": 130, "close": 133},
+    {"open": 165, "high": 177, "low": 153, "close": 161},
+    {"open": 144, "high": 199, "low": 133, "close": 186}
+]
+
+# Calculate average close price.
+def calculateAverageClose(candles): 
+    total = 0 
+
+    for i in range(len(candles)):
+        total += candles[i]["close"]
+
+    return total / len(candles)
+
+print(calculateAverageClose(candles))
